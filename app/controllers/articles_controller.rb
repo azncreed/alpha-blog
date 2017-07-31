cfb8c9a8e8cb334a64f2d932770ae58264df32b8
Sylvia Shen <azncreedii@gmail.com>
2017-07-31 11:11:20 +1000

    fix routes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,12 +1,14 @@
 class ArticlesController < ApplicationController
+
    def new
-   	 @article = Article.new
+   	@article = Article.new
    end
 
    def create
+   	 # render plain: params[:article].inspect
    	 @article = Article.new(article_params)
    	 @article.save
-   	 redirect_to articles_show(@article)
+   	 redirect_to article_path(@article)
    end
 
    private
